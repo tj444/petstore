b34b7a56c4b4758ecc70db494064c48c27ef9d91
luohuafeixue <897304222@qq.com>
2020-06-21 21:43:55 +0800

    Add files via upload

---
+++ petDoctor/app/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "./common/uni.css";
+	@import "./common/reset.css";
+	.mpvue-picker__hd{padding:0!important;}
+	.mpvue-picker__action{height: 100%;padding:9px 15px;}
+	
+</style>
